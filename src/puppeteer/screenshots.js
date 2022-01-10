9adaecfa0488624549618a7d0c47161db7827f73
Eric Swanson <ericis@users.noreply.github.com>
2022-01-10 18:45:30 -0500

    ci: comment on the pr with a url to the build
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/puppeteer/screenshots.js
+++ src/puppeteer/screenshots.js
@@ -5,18 +5,20 @@ const docsHostName = process.env.DOCS_HOSTNAME || "our-hello-docs";
 const docsPortNumber = process.env.DOCS_PORT || 8000;
 const screenshotsPath = process.env.SCREENSHOTS_PATH || "/screenshots";
 
-const captureScreenshot = async (url, screenshotImageFileName) => {
+const captureScreenshot = async (page, url, screenshotImageFileName) => {
   console.log(`GET: ${url}`);
 
   await page.goto(url);
 
-  console.log("Capturing a screenshot...");
+  console.log(`Capturing the screenshot as ${screenshotImageFileName}...`);
 
   await page.screenshot({
     path: `${screenshotsPath}/${screenshotImageFileName}`,
+    type: "png",
+    fullPage: true,
   });
 
-  console.log("Successfully captured a screenshot.");
+  console.log("Successfully captured the screenshot.");
 };
 
 (async () => {
@@ -43,16 +45,65 @@ const captureScreenshot = async (url, screenshotImageFileName) => {
   const baseUrl = `http://${docsHostName}:${docsPortNumber}/hello-cloud`;
 
   const screenshotConfigurations = [
-    { url: baseUrl, image: "hello-cloud.png" },
-    { url: `${baseUrl}/contribute/`, image: "contribute.png" },
+    {
+      url: `${baseUrl}/`,
+      image: "hello-cloud.png",
+    },
+    {
+      url: `${baseUrl}/clouds/`,
+      image: "clouds.png",
+    },
+    {
+      url: `${baseUrl}/devops/`,
+      image: "devops.png",
+    },
+    {
+      url: `${baseUrl}/roadmap/`,
+      image: "roadmap.png",
+    },
+    {
+      url: `${baseUrl}/contribute/`,
+      image: "contribute.png",
+    },
+    {
+      url: `${baseUrl}/comments/`,
+      image: "comments.png",
+    },
+    {
+      url: `${baseUrl}/license/`,
+      image: "license.png",
+    },
+    {
+      url: `${baseUrl}/services/dotnet/`,
+      image: "services-dotnet.png",
+    },
+    {
+      url: `${baseUrl}/services/java-springboot/`,
+      image: "services-java-springboot.png",
+    },
+    {
+      url: `${baseUrl}/services/kotlin-springboot/`,
+      image: "services-kotlin-springboot.png",
+    },
+    {
+      url: `${baseUrl}/services/nodejs-expressjs/`,
+      image: "services-nodejs-expressjs.png",
+    },
   ];
 
-  screenshotConfigurations.forEach(async (screenshotConfiguration) => {
+  for (
+    let screenshotIx = 0;
+    screenshotIx < screenshotConfigurations.length;
+    screenshotIx++
+  ) {
+    const screenshotConfiguration = screenshotConfigurations[screenshotIx];
+
     await captureScreenshot(
+      page,
       screenshotConfiguration.url,
       screenshotConfiguration.image
     );
-  });
+  }
 
   console.log("Closing the browser...");
 
